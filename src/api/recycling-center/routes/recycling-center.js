26251c048c9f0547696b8a274404b1295f5ec071
Pranav K Bhaskar <pkb19032002@gmail.com>
2023-06-09 07:04:10 +0530

    First Commit

---
+++ src/api/recycling-center/routes/recycling-center.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * recycling-center router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::recycling-center.recycling-center');
---
+++ src/api/recycling-center/services/recycling-center.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * recycling-center service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::recycling-center.recycling-center');
